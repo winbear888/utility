7849b69f9ac8cb3d778804d9f89196807da1ed96
pakin <wirojwatanakul@gmail.com>
2021-02-23 21:29:09 -0600

    cordon/uncordon aks and gke node pool scripts

--- kubernetes/aks/cordon_node_pool.sh
+++ kubernetes/aks/cordon_node_pool.sh
@@ -1,9 +1,9 @@
 usage() {
   echo ""
-  echo "Cordon GKE nodepool"
+  echo "Cordon AKS nodepool"
   echo 
   echo "Usage:"
-  echo "  cordon_node_pool.sh  [OPTION] project_id=PROJECT_ID cluster_name=CLUSTER_NAME node_pool_name=NODE_POOL_NAME region=REGION"
+  echo "  cordon_node_pool.sh  [OPTION] rg_name=RESOURCE_GROUP_NAME cluster_name=CLUSTER_NAME node_pool_name=NODE_POOL_NAME"
   echo "Default:"
   echo "  disables auto scaling and cordons the node pool NODE_POOL_NAME."
   echo "Options:"
@@ -34,17 +34,14 @@ while [ $# -gt 0 ]; do
     -max_node=*)
       max_node="${1#*=}"
       ;;
-    project_id=*)
-      project_id="${1#*=}"
+    rg_name=*)
+      rg_name="${1#*=}"
       ;;
     cluster_name=*)
       cluster_name="${1#*=}"
       ;;
     node_pool_name=*)
       node_pool_name="${1#*=}"
-      ;;
-    region=*)
-      region="${1#*=}"
       ;;
     -help)
       usage
@@ -58,8 +55,8 @@ while [ $# -gt 0 ]; do
   shift 1
 done
 
-if [ -z "$project_id" ]; then
-  echo "missing project_id"
+if [ -z "$rg_name" ]; then
+  echo "missing rg_name"
   usage
   exit 1
 fi
@@ -95,18 +92,18 @@ if [[ "${auto_scale}" == "true" ]]; then
   fi
 fi
 
-gcloud container clusters get-credentials ${cluster_name} --region ${region} --project ${project_id}
+az aks get-credentials -g ${rg_name} -n ${cluster_name}
 
 if [[ "${mode}" == "cordon" ]]; then
   echo "disable cluster autoscaling for node pool"
-  gcloud container clusters update ${cluster_name} --no-enable-autoscaling --node-pool ${node_pool_name} --region ${region} --project ${project_id}
+  az aks nodepool update --cluster-name ${cluster_name} --name ${node_pool} --resource-group ${rg_name} --disable-cluster-autoscaler
   echo "cordoning node pool"
-  kubectl cordon $(kubectl get nodes -l cloud.google.com/gke-nodepool=${node_pool_name} -o=name | cut -d '/' -f2)
+  kubectl cordon $(kubectl get nodes -l agentpool=${node_pool} -o=name | cut -d '/' -f2)
 else 
   if [[ "${auto_scale}" == "true" ]]; then
     echo "enable cluster autoscaling for node pool"
-    gcloud container clusters update ${cluster_name} --enable-autoscaling --node-pool ${node_pool_name} --region ${region} --project ${project_id} --max-nodes ${max_node} --min-nodes ${min_node}
+    az aks nodepool update --cluster-name ${cluster_name} --name ${node_pool} --resource-group ${rg_name} --enable-cluster-autoscaler --max-count $max_node --min-count $min_node
   fi
   echo "uncordoning node pool"
-  kubectl uncordon $(kubectl get nodes -l cloud.google.com/gke-nodepool=${node_pool_name} -o=name | cut -d '/' -f2)
+  kubectl uncordon $(kubectl get nodes -l agentpool=${node_pool} -o=name | cut -d '/' -f2)
 fi
